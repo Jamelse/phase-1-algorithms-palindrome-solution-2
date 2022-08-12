cc64c6816fc02fd110cbe139125e7836c9bb28aa
Jacob <jacobamelse@hotmail.com>
2022-08-12 14:58:48 -0500

    commit tree da3820fb8887923dff93678b287f3402825936c2

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+// 
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+      const endIndex = word.length - 1 - startIndex;
+      if (word[startIndex] !== word[endIndex]) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+
+
 
 /* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
+  iterate through middle of string. compare letters each other.
+  if letters don't match return false
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome('level'));
+
+  console.log('');
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
